69879e5141ea7c84c5c643ae177cd9850bdcdfa8
Aliyev Amin Allahyar <Aliy_fl50@itstep.edu.az>
2021-05-06 17:48:26 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persons persons = new Persons() { Names="Amin"};
+            Persons persons2 = new Persons() { Names = "Ali" };
+            Persons persons3 = new Persons() { Names = "Akbar" };
+
+            Singletone.Instance.Add(persons);
+            Singletone.Instance.Add(persons2);
+            Singletone.Instance.Add(persons3);
+            Singletone.Instance.Show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/Singletone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+     public sealed class Singletone
+    {
+        static private Singletone instance;
+        private Singletone()
+        {
+
+        }
+        static public Singletone Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singletone();
+
+                }
+                return instance;
+            }
+        }
+        private  List<Persons> _persons { get; set; }
+        public void Add(Persons person)
+        {
+            if (_persons == null)
+            {
+                _persons = new List<Persons>() { };
+            }
+            if (person != null)
+            {
+                _persons.Add(person);
+            }
+        }
+        public void Show()
+        {
+            int i = 1;
+            foreach(var item in _persons)
+            {
+                Console.WriteLine($"{i} : {item.Names}");
+                i++;
+            }
+        }
+     }
+}
